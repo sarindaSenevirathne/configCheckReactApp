22abe3a61bb2ab38848a1c20c81dab6929cb9930
Sarinda Senevirathne <sarindas@sarindas.local>
2024-06-11 16:16:16 -0300

    Adding changes suggested by choreo team

--- public/config.js
+++ public/config.js
@@ -1,7 +1,5 @@
-const config = {
+window.config = {
     apiUrl: "https://api.example.com",
     apiKey: "1234567890abcdef",
     featureToggle: true,
-  };
-  
-  export default config;
+};
--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-// src/App.js
 import React from 'react';
-import config from './config';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Configuration Values</h1>
-        <p><strong>API URL:</strong> {config.apiUrl}</p>
-        <p><strong>API Key:</strong> {config.apiKey}</p>
-        <p><strong>Feature Toggle:</strong> {config.featureToggle ? 'Enabled' : 'Disabled'}</p>
+        <p><strong>API URL:</strong> {window.config.apiUrl}</p>
+        <p><strong>API Key:</strong> {window.config.apiKey}</p>
+        <p><strong>Feature Toggle:</strong> {window.config.featureToggle ? 'Enabled' : 'Disabled'}</p>
       </header>
     </div>
   );
